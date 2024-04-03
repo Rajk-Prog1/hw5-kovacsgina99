430094c96c171de95984522790330abb774961bf
kovacsgina99 <160044245+kovacsgina99@users.noreply.github.com>
2024-04-03 16:18:27 +0000

    sárkányos javítva+kommentelve, tojásos kész

--- tasks/eggs.py
+++ tasks/eggs.py
@@ -1,5 +1,12 @@
-def eggs_solution(breaks):
-    for i in range(1,101):
-        if breaks(i):
-            return i - 1
-    return 100
+def eggs_solution(breaks): # sárkányos feladat logikája mentén
+    low_floor = 0
+    high_floor = 99
+
+    while (low_floor + 1) <= high_floor:
+        middle_floor = low_floor + high_floor // 2
+        if breaks(middle_floor):
+            high_floor = middle_floor - 1
+        else:
+            low_floor = middle_floor + 1
+    
+    return high_floor
