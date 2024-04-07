f9a6b120c080f47ecae80d054b93615e05c96ebc
kovacsgina99 <160044245+kovacsgina99@users.noreply.github.com>
2024-04-07 14:32:21 +0200

    új tojásos 0407

--- tasks/eggs.py
+++ tasks/eggs.py
@@ -10,3 +10,24 @@ def eggs_solution(breaks): # sárkányos feladat logikája mentén
             low_floor = middle_floor + 1
     
     return high_floor
+
+   # új megoldás - próba
+def eggs_solution(breaks): 
+    low_floor = 0
+    high_floor = 99
+
+    while (low_floor + 10) <= high_floor: # 10-esével megyünk végig az emeleteken
+        if low_floor + 10 >= breaks: # megvizsgáljuk, hogy afölött az emelet fölött vagyunk-e, ahol összetört az első tojás
+            break # összetörik a tojás + megtörik a ciklus
+        else:
+            low_floor = low_floor + 10 # ha még nem tört össze, akkor feljebb visszük azt az emeletet, ameddig vizsgálunk
+    while (low_floor + 1) <= high_floor: # egyesével nézzük meg az emeleteket:
+        if low_floor + 1 >= breaks: # legmagasabb emelet, ahol még nem törik össze
+            break # összetörik a tojás + megtörik a ciklus
+        else: 
+            low_floor = low_floor + 1
+
+    return low_floor # legmagasabb szint, ahol még nem törik össze
+print(eggs_solution(10))
+    
+        
