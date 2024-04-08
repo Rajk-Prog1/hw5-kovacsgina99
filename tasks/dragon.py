1e4fd35f24f501118ea692bb1b7d2d7875c4838f
kovacsgina99 <160044245+kovacsgina99@users.noreply.github.com>
2024-04-08 13:05:21 +0000

    dragon javítva 0408

--- tasks/dragon.py
+++ tasks/dragon.py
@@ -2,11 +2,11 @@ def dragon_solution(is_dead, number_of_cows):
     fat_alive_cow_index = 0 # élő tehenek listájának az eleje
     thin_alive_cow_index = number_of_cows - 1 # élő tehenek listájának az utolsó eleme
     
-    while (fat_alive_cow_index + 1) <= thin_alive_cow_index: # bináris keresés
+    while (fat_alive_cow_index) <= thin_alive_cow_index: # bináris keresés
         middle_cow = int((fat_alive_cow_index + thin_alive_cow_index) // 2) # a középső tehenet megkeressük
         if is_dead(middle_cow): # megnézzük, hogy a középső tehén halott-e
-            thin_alive_cow_index = middle_cow - 1 # ha igen, akkor a vékonyabb tehenek oldalán folytatjuk a keresést
+            fat_alive_cow_index = middle_cow + 1 # ha igen, akkor a ducibb tehenek oldalán folytatjuk a keresést
         else:
-            fat_alive_cow_index = middle_cow + 1 # ha nem, akkor a ducibb tehenek oldalán folytatjuk a keresést
+            thin_alive_cow_index = middle_cow - 1 # ha nem, akkor a vékonyabb tehenek oldalán folytatjuk a keresést
     
-    return thin_alive_cow_index # visszaadja a legducibb élő tehenet 
+    return fat_alive_cow_index # visszaadja a legducibb élő tehenet 
